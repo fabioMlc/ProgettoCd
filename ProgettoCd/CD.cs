ccd759f0bb84633d4ed2236a9caa2f8760d73587
Fabio Malucelli <FABIO.MALUCELLI@local.ittcesena.it>
2022-11-05 09:21:07 +0100

    Versione2.2

---
+++ ProgettoCd/CD.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgettoCd
+{
+    public class Cd
+    {
+        public string titolo;
+        public string autore;
+        public List <brano> listaBrani new List<brano>();
+        private int durataTotale;
+
+        public Cd(string TitoloAutore, string NomeAutore, string brano1, string brano2, string brano3)
+        {
+            titolo = TitoloAutore;
+            autore = NomeAutore;
+
+            listaBrani.Add(brano1);
+            listaBrani.Add(brano2);
+            listaBrani.Add(brano3);
+
+        }
+       
+        public void setTitolo(string titolo)
+        {
+            this.titolo = titolo;
+        }
+        public void setAutore(string autore)
+        {
+            this.autore = autore;
+        }
+        public string getTitolo()
+        {
+            return titolo;
+        }
+        public string getAutore()
+        {
+            return autore;
+        }
+        public int toString()
+        {
+            foreach (var brano in listaBrani)
+            {
+                durataTotale = durataTotale + brano.getTitolo() + " ";
+            }
+            return durataTotale;
+        }
+
+        public int Durata()
+        {
+            foreach(var brano in listaBrani)
+            {
+                durataTotale = durataTotale + brano.getDurata();
+            }
+            return durataTotale;
+        }
+
+
+    }
+}
